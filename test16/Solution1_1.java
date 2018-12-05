8412f2300d6beb8bdc267e0a147f8784c3c25138
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:46:35 +0800

    Update and rename Solution1.java to Solution1_1.java

--- test16/Solution1_1.java
+++ test16/Solution1_1.java
@@ -2,7 +2,7 @@ package test16;
 /**
  * 删除链表中重复的结点
  * 
- * ① 如果该节点不是尾节点，那么可以直接将下一个节点的值赋给该节点，然后令该节点指向下下个节点，再删除下一个节点，时间复杂度为 O(1)。
+ * ① 如果该节点不是尾节点，那么可以直接将下一个节点的值赋给该节点，然后令该节点指向下下个节点，时间复杂度为 O(1)。
  * ② 否则，就需要先遍历链表，找到节点的前一个节点，然后让前一个节点指向 null，时间复杂度为 O(N)。
  * 综上，如果进行 N 次操作，那么大约需要操作节点的次数为 N-1+N=2N-1，
  * 其中 N-1 表示 N-1 个不是尾节点的每个节点以 O(1) 的时间复杂度操作节点的总次数，N 表示 1 个尾节点以 O(N) 的时间复杂度操作节点的总次数。
@@ -12,7 +12,7 @@ package test16;
  */
 import test04.ListNode;
 
-public class Solution1 {
+public class Solution1_1 {
 	public ListNode deleteNode(ListNode head, ListNode tobeDelete) {
 	    if (head == null || head.next == null || tobeDelete == null)
 	        return null;
@@ -21,7 +21,12 @@ public class Solution1 {
 	        ListNode next = tobeDelete.next;
 	        tobeDelete.val = next.val;
 	        tobeDelete.next = next.next;
+	    } else if(head == tobeDelete){
+	    	// 链表只有1个节点
+	    	tobeDelete = null;
+	    	head = null;
 	    } else {
+	    	// 要删除的节点是尾节点
 	        ListNode cur = head;
 	        while (cur.next != tobeDelete)
 	            cur = cur.next;
