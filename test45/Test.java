e7098efb565031c5db69f4caa86f63e2cc773b7c
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 16:57:17 +0800

    Update Test.java

--- test45/Test.java
+++ test45/Test.java
@@ -2,12 +2,18 @@ package test45;
 
 public class Test {
 	public static void main(String[] args) {
-		Solution solution = new Solution();
-		 int[][] values = {
+		Solution1 solution1 = new Solution1();
+		Solution2 solution2 = new Solution2();
+/*		 int[][] values = {
 	                {1, 2, 3},
 	                {4, 5, 6},
-	                {7, 8, 9}};
-	    System.out.println(solution.getMaxPathValue(values));  //29
-	    System.out.println('r'-'a');
+	                {7, 8, 9}};*/
+		int[][] values = {
+				{1,10,3,8},
+				{12,2,9,6},
+				{5,7,4,11},
+				{3,7,16,5}};
+	  //System.out.println(solution1.getMaxPathValue(values));  //29
+	    System.out.println(solution2.getMaxPathValue(values));  //29
 	}
 }
