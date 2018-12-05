efeafd493958b18361f9a39394939139b297ef6e
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 17:43:39 +0800

    Update Test.java

--- test01/Test.java
+++ test01/Test.java
@@ -1,18 +1,34 @@
 package test01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {	
 		Soluction soluction = new Soluction();
 	    int numbers[] = {2,3,1,0,2,5,3};
 	    int duplication[] = new int[1];
-	    int output;
+	    //找出数组中任意一个重复的数字
         boolean bool = soluction.duplicate(numbers, numbers.length, duplication);
         System.out.println(bool + " "+ duplication[0]);
+        
+        //找出所有的重复数字
+        List<Integer> list = new ArrayList<>();
+        boolean duplicateAll = soluction.duplicateAll(numbers, numbers.length, list);
+        System.out.println(duplicateAll);
+        for (Integer integer : list) {
+        	System.out.println(integer);
+		}
+        System.out.println("--------");
+        
+        int nums[] = {2,3,1,4,2,5,3};
+        List<Integer> arraylist = new ArrayList<>();
+        soluction.duplicateNotModify(nums, nums.length, arraylist);
+        for (Integer integer : arraylist) {
+			System.out.println(integer);
+		}
 	    
 	}
-
     
-    
-
 }
