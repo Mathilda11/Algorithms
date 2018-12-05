667a40f37632437ac7b0cda92f72775b9772f17c
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:31:06 +0800

    Update Solution1_2.java

--- test12/Solution1_2.java
+++ test12/Solution1_2.java
@@ -11,7 +11,12 @@ public class Solution1_2 {
 	    dp[1] = 1;
 	    for (int i = 2; i <= n; i++)
 	        for (int j = 1; j < i; j++)
+	        	//在n<8时，两种计算结果是相同的，因为dp[5]=6
+	        	//当n=8时，上：dp[8]=dp[5]*3=18，下：dp[8]=4*4=16。
+	        	//所以要保留dp[j]
+	        	//在求dp[i]之前，对于每个j(0<j<i)而言，dp[j]都已经求解出来了，结果保存在dp[j]。
 	            dp[i] = Math.max(dp[i], Math.max(j * (i - j), dp[j] * (i - j)));
+	        	//dp[i] = Math.max(dp[i], (j * (i - j)));
 	    return dp[n];
 	}
 }
