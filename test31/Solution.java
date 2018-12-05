e32d9fde46d8e18b68a55f7e43f5dc67334001a3
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 22:31:19 +0800

    Update Solution.java

--- test31/Solution.java
+++ test31/Solution.java
@@ -2,9 +2,9 @@ package test31;
 
 /**
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。假设输入的数组的任意两个数字都互不相同。
- * 例如，下图是后序遍历序1,3,2 所对应的二叉搜索树。
- * 于一个二叉树的后序遍历序列来说，最后一个数一定是根节点，然后前面的数中，从最开始到第一个大于根节点的数都是左子树中的数，
- * 而后面到倒数第二个数应该都是大于根节点的，是右子树，如果后面的数中有小于根节点的，那么说明这个序列不是二叉搜索树的后序遍历序列
+ * 
+ * 于一个二叉树的后序遍历序列来说，最后一个数一定是根节点，然后前面的数中，从最开始到第一个大于根节点的数-1都是左子树中的数，
+ * 而第一个大于根节点的数到倒数第二个数应该都是大于根节点的，是右子树，如果后面的数中有小于根节点的，那么说明这个序列不是二叉搜索树的后序遍历序列
  * @author 54060
  *
  */
@@ -22,9 +22,10 @@ public class Solution {
 	    int cutIndex = first;
 	    while (cutIndex < last && sequence[cutIndex] <= rootVal)
 	        cutIndex++;
+	    //此时得到的cutIndex是小于根节点的个数+一个大于根节点的个数。
 	    for (int i = cutIndex; i < last; i++)
-	        if (sequence[i] < rootVal)
+	        if (sequence[i] < rootVal)  //右子树中有比根节点小的，则违背了二叉搜索树的定义。
 	            return false;
-	    return verify(sequence, first, cutIndex - 1) && verify(sequence, cutIndex, last - 1);
+	    return verify(sequence, first, cutIndex - 1) && verify(sequence, cutIndex, last - 1);//左子树和右子树都是二叉搜索树才返回true
 	}
 }
