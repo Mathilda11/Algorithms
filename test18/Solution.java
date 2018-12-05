8f53a4fb9cdcd29c0b979b3b68d79da511c52430
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:54:23 +0800

    Update Solution.java

--- test18/Solution.java
+++ test18/Solution.java
@@ -1,6 +1,22 @@
 package test18;
 
 /**
+ * 
+        true
+	
+	"+100"
+	"5e2"
+	"-123"
+	"3.1416"
+	"-1E-16"
+
+	false
+
+	"12e"
+	"1a3.14"
+	"1.2.3"
+	"+-5"
+	"12e+4.3"
  * 表示数值的字符串
  * []  ： 字符集合
  * ()  ： 分组
@@ -18,6 +34,21 @@ public class Solution {
 	    if (str == null || str.length == 0)
 	        return false;
 	    //在java和python中 .2也是数字
+	    //*：0次，1次或多次
+	    //+：1次或多次
+	    //?：0次或1次
+	    //表示数值的字符串遵循模式A.[.[B]][E|eC]或.B[E|eC]
 	    return new String(str).matches("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");//()?表示是可选的
 	}
+	
+	//使用java中异常的方式
+	public static boolean isNumeric(String str) { 
+		try { 
+			double res = Double.parseDouble(str); 
+		} catch (Exception e) { 
+			return false; 
+		} 
+		return true; 
+		}
+
 }
