21f1169916ad2c8e9430ec8c2770c6e06792f6bc
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:21:53 +0800

    Update Test.java

--- test09/Test.java
+++ test09/Test.java
@@ -3,7 +3,8 @@ package test09;
 public class Test {
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		int[] nums = {0,0,1,1,1};
+		//int[] nums = {3,4,5,1,2};
+		int[] nums = {1,1,1,0,1};
 		int minNumberInRotateArray = solution.minNumberInRotateArray(nums);
 		System.out.println(minNumberInRotateArray);
 	}
