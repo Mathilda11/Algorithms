407eae8e4acb7a6ad1fda09c88755516a3f81c94
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 19:23:30 +0800

    Update Test.java

--- test05/Test.java
+++ test05/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		int[] preorder = {3,9,20,15,7};
 		int[] inorder =  {9,3,15,20,7};
-		Solution solution = new Solution();
+		Solution2 solution = new Solution2();
 		TreeNode treeNode = solution.reConstructBinaryTree(preorder, inorder);
 	}
 
