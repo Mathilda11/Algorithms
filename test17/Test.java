7a8ea8b496a351058ba0fe60c79f6215735d4074
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:51:01 +0800

    Update Test.java

--- test17/Test.java
+++ test17/Test.java
@@ -2,9 +2,9 @@ package test17;
 
 public class Test {
 	public static void main(String[] args) {
-		Solution1 solution1 = new Solution1();
+		Solution1_2 solution = new Solution1_2();
 		char[] str = {'a','a','a'};
 		char[] pattern = {'a','.','a'};
-		solution1.match(str, pattern);
+		solution.match(str, pattern);
 	}
 }
