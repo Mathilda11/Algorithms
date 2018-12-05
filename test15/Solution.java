2028579c090f68a3e05e834d67951254b98059b4
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:37:14 +0800

    Update Solution.java

--- test15/Solution.java
+++ test15/Solution.java
@@ -14,18 +14,27 @@ public class Solution {
 	    char[] number = new char[n];
 	    print1ToMaxOfNDigits(number, 0);
 	}
-
+	
+	/*
+	 * [0,0,0] == > 使第2位从0~9遍历，打印完一位数
+	 * [0,1,0] == > [0,1,0~9]...[0,9,0~9]打印完两位数
+	 * [1,0,0] == > [1,1,0~9]...[1,9,0~9]  [9,1,0~9]...[9,9,0~9]打印完三位数
+	 */
+	//全排列。数字的每一位都可能是0~9中的一个数，然后设置下一位。
+	//递归结束的条件是已经设置了数字的最后一位。
 	private void print1ToMaxOfNDigits(char[] number, int digit) {
-	    if (digit == number.length) {
+	    if (digit == number.length) {	//digit位都填上后开始打印
 	        printNumber(number);
 	        return;
 	    }
 	    for (int i = 0; i < 10; i++) {
-	        number[digit] = (char) (i + '0');
+	        number[digit] = (char) (i + '0');	//(char) (i + '0') = 'i';
 	        print1ToMaxOfNDigits(number, digit + 1);
 	    }
+	    //000~009，019->p(019,2)->010->p(010,3)
 	}
-
+	
+	//index确定从n位数的哪一位开始打印。比如001，index=2，打印1后换行
 	private void printNumber(char[] number) {
 	    int index = 0;
 	    while (index < number.length && number[index] == '0')
