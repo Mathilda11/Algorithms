64536b9c601a88325ae7241b36030938c27aba72
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-05 20:48:38 +0800

    Update Solution2.java

--- test16/Solution2.java
+++ test16/Solution2.java
@@ -4,13 +4,14 @@ package test16;
  * 删除链表中重复的结点
  * 
  * 思路：头结点和第二个结点比较，如果值相等的话，跳过第二个结点，头使结点依次和其他剩余结点比较，若有值相等，则跳过。
- * 第一个结点和第二个结点比较，不等，将第二个结点作为头结点传入函数本函数
+ * 第一个结点和第二个结点比较，不等，将第二个结点作为头结点传入本函数
  * @author 54060
  *
  */
 import test04.ListNode;
 
 public class Solution2 {
+	//1->2->2->3->3->4
 	public ListNode deleteDuplication(ListNode pHead) {
 	    if (pHead == null || pHead.next == null){
 	        return pHead;
@@ -18,11 +19,11 @@ public class Solution2 {
 	    ListNode next = pHead.next;
 	    if (pHead.val == next.val) {
 	        while (next != null && pHead.val == next.val){
-	            next = next.next;
+	            next = next.next; //next=3  next=4
 	        }
-	        return deleteDuplication(next);
+	        return deleteDuplication(next);//d(3->3->4) d(4)
 	    } else {
-	        pHead.next = deleteDuplication(pHead.next);
+	        pHead.next = deleteDuplication(pHead.next);//1->d(2->2->3->3->4) 1->4
 	        return pHead;
 	    }
 	}
