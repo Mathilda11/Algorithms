4be9e4076b2452ed1d4240e330beb605eb9b43b5
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-06 15:21:44 +0800

    Update Solution2.java

--- test51/Solution2.java
+++ test51/Solution2.java
@@ -4,6 +4,8 @@ package test51;
  * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0到n-1之内。在范围0到n-1的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
  * 
  * 实质是找出第一个值和下标不相等的元素。
+ 
+ * 时间复杂度O(log n)
  */
 public class Solution2 {
     public int getMissingNumber(int[] nums) {
