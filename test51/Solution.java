9af780c7a39d82a368eaa8cf688f325558978caa
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-06 15:13:41 +0800

    Update Solution.java

--- test51/Solution.java
+++ test51/Solution.java
@@ -1,35 +1,29 @@
 package test51;
-/**
- * 数字在排序数组中出现的次数
- * Input:
- * nums = 1, 2, 3, 3, 3, 3, 4, 6
- * K = 3
- * Output:
- * 4
- * 有序数组的查找利用二分查找，可以查找k和k+1的下标，做差。就是k在数组中出现的次数
-
- * @author 54060
- *
+/*
+ * 0到n-1中缺失的数字
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0到n-1之内。在范围0到n-1的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ * 
+ * 实质是找出第一个值和下标不相等的元素。
  */
-public class Solution {
-	public int GetNumberOfK(int[] nums, int K) {
-	    int first = binarySearch(nums, K);
-	    System.out.println(first);
-	    int last = binarySearch(nums, K + 1);
-	    //first == nums.length 查找的K不存在且大于数组中的最大值 这样查出来的fisrt是数组的长度，不能用nums[first] != K判定，因为存在数组越界问题
-	    //nums[first] != K 查找的K不存在且在数组值的范围内，这样查出来的0<first<length 或 查找的k不存在且小于数组的最小值，这样查出来的first是0
-	    return (first == nums.length || nums[first] != K) ? 0 : last - first;
-	}
-
-	private int binarySearch(int[] nums, int K) {
-	    int l = 0, h = nums.length;
-	    while (l < h) {
-	        int m = l + (h - l) / 2;
-	        if (nums[m] >= K) //注意：>=，为了找到第一个k
-	            h = m;
-	        else
-	            l = m + 1;
-	    }
-	    return l;
-	}
+public class Solution2 {
+    public int getMissingNumber(int[] nums) {
+        if(nums==null || nums.length<=0)
+            return -1;
+        int l=0;
+        int h=nums.length-1;
+        while(l<=h) {
+            int m = l + (h - l)/2;
+            if(nums[m]!=m) { //该元素的值和下标不相等
+                if(m==0 || nums[m-1]==m-1) //如果该元素是第一个或者该元素前面的元素值和下标相等，则该元素就是缺少的那个
+                    return m;
+                h=m-1; //否则，缺少的在前半段
+            }else {  
+                l=m+1; //缺少的在后半段
+            }
+        }
+        if(l==nums.length){  //数组前面的元素值都和下标相等，说明缺失的是最大的那个元素(n-1)
+        	return nums.length;
+        }
+        return -1; //无效的输入
+    }
 }
