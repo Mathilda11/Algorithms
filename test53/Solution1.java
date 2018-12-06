463351026ab0f0018736db52131633e6a8f19a11
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-06 15:34:49 +0800

    Update Solution1.java

--- test53/Solution1.java
+++ test53/Solution1.java
@@ -10,6 +10,9 @@ import test05.TreeNode;
  */
 public class Solution1 {
 	public int TreeDepth(TreeNode root) {
+		//如果一棵树只有一个节点，那么它的深度为1。
+		//如果根节点只有左子树而没有右子树，那么树的深度应该是其左子树的深度+1。
+		//如果根节点既有左子树又有右子树，那么树的深度就是其左、右子树深度的较大值+1。
 	    return root == null ? 0 : 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
 	}
 }
