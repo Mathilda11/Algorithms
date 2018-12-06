e486e4e21b43530a0193d9bfbbc82e12509318cf
日向彻永垂不朽 <33115706+Mathilda11@users.noreply.github.com>
2018-12-06 08:55:58 +0800

    Update Solution.java

--- test40/Solution.java
+++ test40/Solution.java
@@ -8,13 +8,13 @@ package test40;
  */
 public class Solution {
 	public int FindGreatestSumOfSubArray(int[] nums) {
-	    if (nums == null || nums.length == 0)
+	    if (nums == null || nums.length == 0)//考虑无效的输入
 	        return 0;
 	    int greatestSum = Integer.MIN_VALUE;//用来记录前面已经求和过的最大值
 	    int sum = 0;
 	    for (int val : nums) {
-	        sum = sum <= 0 ? val : sum + val; //如果前面的和小于等于0，则重新计算后面的连续子数组
-	        greatestSum = Math.max(greatestSum, sum);//将前面求和过的最大值和新的连续子数组的和比较 取最大的连续和。
+	        sum = sum <= 0 ? val : sum + val; //sum记录上次求和之后的，如果前面的和小于等于0，则重新计算后面的连续子数组
+	        greatestSum = Math.max(greatestSum, sum);//将前面求和过的最大值和新的连续子数组的和比较，greatestSum记录目前为止的最大的连续和。
 	    }
 	    return greatestSum;
 	}
